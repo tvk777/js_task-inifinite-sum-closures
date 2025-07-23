f32931a491aaeb65b9e6b77752d33575a0176a25
tvk777 <klimenko.tv7@gmail.com>
2025-07-23 16:09:57 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,8 +3,25 @@
 /***
  * @return {function}
  */
+/* eslint no-console: ["error", { allow: ["log"] }] */
 function makeInfinityAdder() {
+  let totalResult = 0;
 
+  const sum = (a) => {
+    if (a === undefined) {
+      const localResult = totalResult;
+
+      totalResult = 0;
+
+      return localResult;
+    }
+
+    totalResult += a;
+
+    return sum;
+  };
+
+  return sum;
 }
 
 module.exports = makeInfinityAdder;
